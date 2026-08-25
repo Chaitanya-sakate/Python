ffb31d986ab6ef675e019dbf0ed9e47f24fb7f81
Chaitanya <chaitanyasakate@gmail.com>
2026-08-25 13:05:39 +0530

    modulues : math,random,datetime,keyword and include help function

---
+++ lect2/modulues.py
@@ -0,0 +1,14 @@
+#modulus math
+import math
+print(math.factorial(3))
+#keyword
+import keyword
+print(keyword.kwlist)
+#datetime
+import datetime
+print(datetime.datetime.now())
+#random
+import random
+print(random.randint(1,20))
+#help function
+help('modules')
