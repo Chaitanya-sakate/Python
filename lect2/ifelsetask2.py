ed737cefbf03540f272aace800dc632b4cc01b79
Chaitanya <chaitanyasakate@gmail.com>
2026-08-25 11:08:25 +0530

    menu driven calculator

---
+++ lect2/ifelsetask2.py
@@ -0,0 +1,21 @@
+#menu driven calculator
+fval=int(input('enter number'))
+sval=int(input('enter number'))
+oper=input('enter operation :')
+if oper=='+':
+    print(fval+sval)
+elif oper=='-':
+    print(fval-sval)
+elif oper=='*':
+    print(fval*sval)
+elif oper=='/':
+    print(fval/sval)
+elif oper=='%':
+    print(fval%sval)
+else:
+    print('no operatin you can write')
+    
+
+
+    
+
