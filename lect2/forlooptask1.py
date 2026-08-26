b7d41628c9554dfc82d5f4b0f3a28ee25f358763
Chaitanya <chaitanyasakate@gmail.com>
2026-08-26 11:04:12 +0530

    for loop using datatype

---
+++ lect2/forlooptask1.py
@@ -0,0 +1,12 @@
+#datatype print using for loop-string
+for i in 'chaitanya':
+    print(i)
+#list
+for i in (1,2,3,4):
+    print(i)
+#touple
+for i in ([1,2]):
+    print(i)
+#set
+for b in ({1,2}):
+    print(b)
